7b71f3056b49707d730dc88c6b4af433ea7950a3
kcansari <kcansari0@gmail.com>
2022-09-27 14:47:14 +0300

    Add - Products photo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import {Container} from 'react-bootstrap';
+import HomeScreen from "./Screens/HomeScreen";
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <Header/>
     <main className="py-3">
       <Container>
-        <h1> Welcome to ProShop </h1>
+        <HomeScreen/>
       </Container>
     </main>
     <Footer/>
