6bbb85d49b85c411a9d554ffb9c39f0242edf28b
kcansari <kcansari0@gmail.com>
2022-09-27 17:14:52 +0300

    Add react-router-components - browserRouter, Route, Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,28 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import {Container} from 'react-bootstrap';
 import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
+import {
+  BrowserRouter ,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter >
     <Header/>
     <main className="py-3">
       <Container>
-        <HomeScreen/>
+        <Routes>
+          <Route path="/" element={<HomeScreen/>} exact />
+          <Route path="/product/:id" element={<ProductScreen/>} exact />
+        </Routes>
       </Container>
     </main>
     <Footer/>
-    </>
+    </BrowserRouter>
   );
 }
 
