7d72726581496ed903e03c4c16137c08f9334dea
kcansari <kcansari0@gmail.com>
2022-10-03 17:10:12 +0300

    Add - DB connect method, connected and colors import

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,12 @@ import products from './data/products.js'
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
+import connectDB from './config/db.js'
+import colors from 'colors'
+
+
+connectDB()
+
 
 app.get('/', (req, res) => {
     res.send('ProShop Backend Server')
@@ -24,5 +30,5 @@ app.get('/api/products/:id', (req, res) => {
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-    console.log(`Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 })
