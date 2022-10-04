01b8b701d9223aef003350a358bed0a8529ccf19
kcansari <kcansari0@gmail.com>
2022-10-04 15:00:26 +0300

    Initialize file codes

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: "Admin User",
+        email: "admin@example.com",
+        password: bcrypt.hashSync('123456',10),
+        isAdmin: true
+    },
+    {
+        name: "Kemal Can",
+        email: "kemalcan@example.com",
+        password: bcrypt.hashSync('123456',10),
+    },
+    {
+        name: "Alperen",
+        email: "alperen@example.com",
+        password:bcrypt.hashSync('123456',10),
+    },
+]
+
+export default users
