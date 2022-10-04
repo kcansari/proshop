a23359ea1c023efa5314211562a67083b2c98fda
kcansari <kcansari0@gmail.com>
2022-10-04 21:10:51 +0300

    Add - CartScreen page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from "./Components/Header";
 import {Container} from 'react-bootstrap';
 import HomeScreen from "./Screens/HomeScreen";
 import ProductScreen from "./Screens/ProductScreen";
+import CartScreen from "./Screens/CartScreen";
 import {
   BrowserRouter ,
   Route,
@@ -19,7 +20,9 @@ function App() {
       <Container>
         <Routes>
           <Route path="/" element={<HomeScreen/>} exact />
-          <Route path="/product/:id" element={<ProductScreen/>} exact />
+          <Route path="/product/:id" element={<ProductScreen/>}/>
+          <Route path="/cart/:id" element={<CartScreen/>} />
+          <Route path="/cart/" element={<CartScreen />} />
         </Routes>
       </Container>
     </main>
