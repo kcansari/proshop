853e7e4d865eeba1d1257a63a0887b77c7368846
kcansari <kcansari0@gmail.com>
2022-10-04 15:01:18 +0300

    Add - productRoutes, notFound,  errorHandler

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,23 @@
 import express from 'express'
-const app = express()
-
-
-import products from './data/products.js'
-
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
-
 import connectDB from './config/db.js'
 import colors from 'colors'
+import productRoutes from './routes/productRoutes.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
-
+dotenv.config()
+const app = express()
 connectDB()
-
 
 app.get('/', (req, res) => {
     res.send('ProShop Backend Server')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product  = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
