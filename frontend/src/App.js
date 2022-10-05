32cda8bc7a29467c0de7c75fba4537f9b274a55c
kcansari <kcansari0@gmail.com>
2022-10-05 17:44:09 +0300

    Delete - HomeScreen cart button path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -21,8 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomeScreen/>} exact />
           <Route path="/product/:id" element={<ProductScreen/>}/>
-          <Route path="/cart/:id" element={<CartScreen/>} />
-          <Route path="/cart/" element={<CartScreen />} />
+          <Route path="/cart/:id/:qty" element={<CartScreen/>} />
         </Routes>
       </Container>
     </main>
