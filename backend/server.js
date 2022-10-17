b1cb14442f199595a85a08e580f7af73e48fbb8c
kcansari <kcansari0@gmail.com>
2022-10-17 16:29:03 +0300

    Add- userRoutes & Format prettier

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,22 @@ import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-d
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import userRoutes from './routes/userRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 const app = express()
 connectDB()
 
+// Allow us to accept JSON data in the body.
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    res.send('ProShop Backend Server')
+  res.send('ProShop Backend Server')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 app.use(notFound)
 
@@ -22,5 +27,7 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
-    console.log(`Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Sever running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 })
