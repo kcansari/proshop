e2f108a1c8295d76901d5342832b235493215508
kcansari <kcansari0@gmail.com>
2022-10-17 16:32:21 +0300

    get method moved another file, this file was cleaned

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,30 +1,12 @@
-import express from "express"
+import express from 'express'
 const router = express.Router()
-import Product from "../models/productModel.js"
-import asyncHandler from 'express-async-handler'
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js'
 
-// @desc Fetch all products
-// @route GET /api/products
-// @access Public
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    // res.status(401)
-    // throw new Error("Not Authorized")
-    res.json(products)
-}))
+router.route('/').get(getProducts)
 
-// @desc Fetch single products
-// @route GET /api/products/:id
-// @access Public
-router.get('/:id',asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (product) {
-        res.json(product)
-    } else {
-        // res.status(404).json({message: 'Product not found'})
-        res.status(404)
-        throw new Error("Product not found")
-    }
-}))
+router.route('/:id').get(getProductById)
 
 export default router
