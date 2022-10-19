e00d2d19a4b5526d0349340deafb1ff56af26c4f
kcansari <kcansari0@gmail.com>
2022-10-19 16:15:58 +0300

    Add- User Login Screen & Functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,30 @@
-import React from "react";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import {Container} from 'react-bootstrap';
-import HomeScreen from "./Screens/HomeScreen";
-import ProductScreen from "./Screens/ProductScreen";
-import CartScreen from "./Screens/CartScreen";
-import {
-  BrowserRouter ,
-  Route,
-  Routes,
-} from "react-router-dom";
-
+import React from 'react'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './Screens/HomeScreen'
+import ProductScreen from './Screens/ProductScreen'
+import CartScreen from './Screens/CartScreen'
+import LoginScreen from './Screens/LoginScreen'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter >
-    <Header/>
-    <main className="py-3">
-      <Container>
-        <Routes>
-          <Route path="/" element={<HomeScreen/>} exact />
-          <Route path="/product/:id" element={<ProductScreen/>}/>
-          <Route path="/cart/:id/:qty" element={<CartScreen/>} />
-        </Routes>
-      </Container>
-    </main>
-    <Footer/>
+    <BrowserRouter>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/login' element={<LoginScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+            <Route path='/cart/:id/:qty' element={<CartScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/Screens/LoginScreen.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Form, Button, Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../Components/Message.js'
+import Loader from '../Components/Loader.js'
+import { login } from '../actions/userActions.js'
+import FormContainer from '../Components/FormContainer'
+import Stack from 'react-bootstrap/Stack'
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const dispatch = useDispatch()
+  const userLogin = useSelector((state) => state.userLogin)
+  const { loading, error, userInfo } = userLogin
+
+  const redirect = window.location.search
+    ? window.location.search.split('=')[1]
+    : '/'
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (userInfo) {
+      navigate(redirect)
+    }
+  }, [navigate, userInfo])
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(login(email, password))
+  }
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type='submit' variant='primary' className='mt-2'>
+          Sign In
+        </Button>
+      </Form>
+      <Row className='py-3'>
+        <Col>
+          New Customer?{' '}
+          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
+            Register
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  )
+}
+
+export default LoginScreen
