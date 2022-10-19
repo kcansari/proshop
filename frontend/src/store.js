891a86be1a3299c809ec4df953e4eb103a09e1bd
kcansari <kcansari0@gmail.com>
2022-10-19 15:03:18 +0300

    Add - User Login Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,26 +1,40 @@
-import {combineReducers, applyMiddleware } from "redux"
-import thunk from "redux-thunk"
-import {composeWithDevTools} from "redux-devtools-extension"
+import { combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { configureStore } from '@reduxjs/toolkit'
-import {productListReducer, productDetailsReducer} from './reducers/productReducers.js'
-import { carReducer } from "./reducers/cartReducers.js"
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers.js'
+import { carReducer } from './reducers/cartReducers.js'
+import { userLoginReducer } from './reducers/userReducers.js'
 
 const reducer = combineReducers({
-    productList:productListReducer,
-    productDetails:productDetailsReducer,
-    cart:carReducer
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: carReducer,
+  userLogin: userLoginReducer,
 })
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') 
-                             ? JSON.parse(localStorage.getItem('cartItems'))
-                             : []
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : []
+
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
 
 const initialState = {
-    cart: {cartItems: cartItemsFromStorage},
+  cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middleware = [thunk]
 
-const store = configureStore({reducer:reducer},initialState,composeWithDevTools(applyMiddleware(...middleware)))
+const store = configureStore(
+  { reducer: reducer },
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
 
 export default store
