a23d4606ba1702af6fd6fcdd7f68ae33f4d4430b
kcansari <kcansari0@gmail.com>
2022-10-24 13:10:12 +0300

    Admin User List

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ShippingScreen from './Screens/ShippingScreen'
 import PaymentScreen from './Screens/PaymentScreen'
 import PlaceOrderScreen from './Screens/PlaceOrderScreen'
 import OrderScreen from './Screens/OrderScreen'
+import UserListScreen from './Screens/UserListScreen'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -31,6 +32,7 @@ function App() {
             <Route path='/payment' element={<PaymentScreen />} />
             <Route path='/placeorder' element={<PlaceOrderScreen />} />
             <Route path='/order/:id' element={<OrderScreen />} />
+            <Route path='/admin/userlist' element={<UserListScreen />} />
           </Routes>
         </Container>
       </main>
---
+++ frontend/src/Screens/UserListScreen.js
@@ -0,0 +1,80 @@
+import React, { useEffect } from 'react'
+// import { useNavigate } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Table, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../Components/Message.js'
+// import Loader from '../Components/Loader.js'
+import { listUsers } from '../actions/userActions.js'
+
+const UserListScreen = () => {
+  const dispatch = useDispatch()
+
+  const userList = useSelector((state) => state.userList)
+  const { loading, error, users } = userList
+
+  const deleteHandler = (id) => {
+    console.log('delete')
+  }
+
+  useEffect(() => {
+    dispatch(listUsers())
+    // eslint-disable-next-line
+  }, [dispatch])
+  return (
+    <>
+      <h1>Users</h1>
+      {loading ? (
+        <loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className='table-sm'>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NAME</th>
+              <th>EMAIL</th>
+              <th>ADMIN</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>
+                  <a href={`mailto:${user.email}`}>{user.email}</a>
+                </td>
+                <td>
+                  {user.idAdmin ? (
+                    <i className='fas fa-check' style={{ color: 'green' }}></i>
+                  ) : (
+                    <i className='fas fa-times' style={{ color: 'red' }}></i>
+                  )}
+                </td>
+                <td>
+                  <LinkContainer to={`/user/${user._id}/edit`}>
+                    <Button variant='light' className='btn-sm'>
+                      <i className='fas fa-edit'></i>
+                    </Button>
+                  </LinkContainer>
+                  <Button
+                    variant='danger'
+                    className='btn-sm'
+                    onClick={() => deleteHandler(user._id)}
+                  >
+                    <i className='fas fa-trash'></i>
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  )
+}
+
+export default UserListScreen
