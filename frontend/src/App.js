506e11ce14b33f77547570ebda96654bbfa420f3
kcansari <kcansari0@gmail.com>
2022-10-25 18:00:27 +0300

    Edit Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from './Screens/OrderScreen'
 import UserListScreen from './Screens/UserListScreen'
 import UserEditScreen from './Screens/UserEditScreen'
 import ProductListScreen from './Screens/ProductListScreen'
+import ProductEditScreen from './Screens/ProductEditScreen'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -37,6 +38,10 @@ function App() {
             <Route path='/admin/userlist' element={<UserListScreen />} />
             <Route path='/admin/user/:id/edit' element={<UserEditScreen />} />
             <Route path='/admin/productlist' element={<ProductListScreen />} />
+            <Route
+              path='/admin/product/:id/edit'
+              element={<ProductEditScreen />}
+            />
           </Routes>
         </Container>
       </main>
